b10953475cf0dae2480c91137dc1c25a5e7a6224
Cimelody210 <144228743+Cimelody210@users.noreply.github.com>
2023-10-06 16:56:16 +0700

    Delete TinhGiaiThua.py

--- TinhGiaiThua.py
+++
@@ -1,7 +0,0 @@
-def TinhGiaiThua():
-    n = int(input('Nhập số cần tính giai thừa: '))
-    kq_giaithua=1
-    for i in range(1, n+1):
-        kq_giaithua=kq_giaithua*i
-    print("Kết quả n giai thừa là: ", kq_giaithua)
-TinhGiaiThua()
