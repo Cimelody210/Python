8869f0b116fe7153eddf47e44361748d8ef1c47a
Cimelody210 <144228743+Cimelody210@users.noreply.github.com>
2023-10-26 10:48:56 +0700

    Delete BaiTapLamThem/Phản chiếu ảnh/namnhuan.py

--- BaiTapLamThem/Phản chiếu ảnh/namnhuan.py
+++
@@ -1,7 +0,0 @@
-# Cách tạo hình phản chiếu qua gương
-
-from PIL import Image
-img = Image.open('Lab2-OOP\TraPhong\Snake.jpg')
-phanchieu = img.transpose(Image.FLIP_LEFT_RIGHT)
-phanchieu.save(r'Lab2-OOP\TraPhong\result.jpg')
-Image.open('Lab2-OOP\TraPhong\result.png')
