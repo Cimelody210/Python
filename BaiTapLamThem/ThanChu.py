47a89eb73f1ea10ae3c123fb7b856e950b558abc
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-06-04 15:34:41 +0700

    Thần chú fix bug

---
+++ BaiTapLamThem/ThanChu.py
@@ -0,0 +1,9 @@
+import os
+try:
+    pass
+    # abchfd
+except Exception as e:
+    arror= str(e)
+    arror  = arror.replace(" ", "_")
+
+    os.sytem(f"xdg-open https://phind.com/search?q=like-a-aprofessional_pytohnproggrammer_fix_this_execption_for_me:{arror}")
